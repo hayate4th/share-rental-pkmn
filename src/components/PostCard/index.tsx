e2ec8a5e06d9b588fa9089bd9949fb3038c37eaf
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-26 19:35:35 +0900

    PostCardで使用するプロパティを定義

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Moment } from "moment";
 
-const PostCard: React.FC = () => {
-  return <div />;
+interface PostCardProps {
+  userName: string;
+  userIconUrl: string;
+  createdAt: Moment;
+  postTitle: string;
+  pkmnImgUrl: string;
+  pkmnPartyDescription: string;
+  tweetUrl: string;
+}
+
+const PostCard: React.FC<PostCardProps> = () => {
+  return (
+    <div>
+      <div>ユーザー名</div>
+      <img src="http://placehold.jp/100x100.png" />
+      <div>投稿日時</div>
+      <div>タイトル</div>
+      <img src="http://placehold.jp/500x300.png" alt="" />
+      <div>パーティの説明</div>
+      <button>いいね</button>
+      <button>レビュー</button>
+      <button>お気に入り</button>
+      <div>参照ツイート</div>
+    </div>
+  );
 };
 
 export default PostCard;
